c2c921214a851711bb086c89ae6c99a9e7061937
khushpanchal <khush.panchal@mxplayer.in>
2021-08-02 12:21:38 +0530

    API Call using OkHttp

--- app/src/main/java/com/example/newzz/MainActivity.java
+++ app/src/main/java/com/example/newzz/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.newzz;
 import android.annotation.SuppressLint;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -15,9 +16,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
@@ -29,9 +28,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.Headers;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import okhttp3.ResponseBody;
+import okhttp3.internal.http2.Http2Reader;
 
 //  LoaderManager.LoaderCallbacks<List<News>>
 public class MainActivity extends AppCompatActivity implements NewsAdapter.NewsListner {
@@ -39,6 +43,9 @@ public class MainActivity extends AppCompatActivity implements NewsAdapter.NewsL
     private List<News> mNewsList;
     private NewsAdapter mNewsAdapter;
     private static final int LOADER_ID = 0;
+    private String mjson;
+
+    private final OkHttpClient client = new OkHttpClient();
 
     private static final String JSON_RESPONSE = "https://newsapi.org/v2/top-headlines?country=in&apiKey=c3a342cc6d394cb19fc3db635741e77f";
 
@@ -59,37 +66,69 @@ public class MainActivity extends AppCompatActivity implements NewsAdapter.NewsL
         makeHttpRequest(JSON_RESPONSE);
     }
 
-    private void makeHttpRequest(String url) {
+    private void makeHttpRequest(String jsonResponse) {
 
-        RequestQueue queue = Volley.newRequestQueue(this);
+        Request request = new Request.Builder()
+                .url(jsonResponse)
+                .build();
 
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                try {
-                    mNewsList = QueryUtils.extractFeatureFromJSON(response);
-                    mNewsAdapter.setNewsData(mNewsList);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
+        client.newCall(request).enqueue(new Callback() {
+            @Override public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
                 Log.i("MainActivity", "That didn't work!");
             }
-        }){
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                Map<String, String> headerMap = new HashMap<String, String>();
-                headerMap.put("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:221.0) Gecko/20100101 Firefox/31.0");
-                return headerMap;
+
+            @Override public void onResponse(Call call, Response response) throws IOException {
+                try (ResponseBody responseBody = response.body()) {
+                    if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+                    mjson = responseBody.string();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                mNewsList = QueryUtils.extractFeatureFromJSON(mjson);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                            mNewsAdapter.setNewsData(mNewsList);
+                        }
+                    });
+                }
             }
-        };
+        });
+    }
+
+//    private void makeHttpRequest(String url) {
+//
+//        RequestQueue queue = Volley.newRequestQueue(this);
+//
+//        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+//            @Override
+//            public void onResponse(String response) {
+//                try {
+//                    mNewsList = QueryUtils.extractFeatureFromJSON(response);
+//                    mNewsAdapter.setNewsData(mNewsList);
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                Log.i("MainActivity", "That didn't work!");
+//            }
+//        }){
+//            @Override
+//            public Map<String, String> getHeaders() throws AuthFailureError {
+//                Map<String, String> headerMap = new HashMap<String, String>();
+//                headerMap.put("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:221.0) Gecko/20100101 Firefox/31.0");
+//                return headerMap;
+//            }
+//        };
 
         // Add the request to the RequestQueue.
-        queue.add(stringRequest);
-    }
+//        queue.add(stringRequest);
+//    }
 
 //    @NonNull
 //    @SuppressLint("StaticFieldLeak")
